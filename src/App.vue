de2aef629805870c364a5d57bbf4b5a56f3469b9
Pumastd <minevskiia10@gmail.com>
2024-02-28 21:34:14 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import Header from "./components/Header.vue";
+export default  {
+    data () {
+        return {
+            a: "A"
+        }
+    }
+}
+
+
+
+</script>
+
+<template>
+
+
+    
+        {{ a }}
+   <Header></Header>
+   
+
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>iH</h1>
+</template>
+<script>
+
+
+</script>
+<style>
+    header {
+        background-color: red;
+        height: 100px;
+    }
+</style>
